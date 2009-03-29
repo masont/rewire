64712f799ced75bf1d2d67eefc4f558b5e85953e
mmmmtang <mmmmtang@14876ee8-1428-11de-b413-eb0469710719>
2009-03-29 16:50:02 +0000

    Added a stub physics engine.

--- src/edu/mit/rewire/model/Item.java
+++ src/edu/mit/rewire/model/Item.java
@@ -2,6 +2,8 @@ package edu.mit.rewire.model;
 
 public interface Item {
     
+    String getType();
+    
     String getTitle();
     
     String getBody();
--- src/edu/mit/rewire/view/Main.java
+++ src/edu/mit/rewire/view/Main.java
@@ -3,6 +3,9 @@ package edu.mit.rewire.view;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.mit.rewire.model.Item;
+import edu.mit.rewire.model.MockItem;
+
 import processing.core.PApplet;
 
 public class Main extends PApplet {
@@ -10,18 +13,23 @@ public class Main extends PApplet {
     private static final long serialVersionUID = -4669039170458943974L;
     
     private final List<Drawable> elements = new ArrayList<Drawable>();
+    private final PhysicsEngine physicsEngine = new PhysicsEngine();
     
     @Override
     public void setup() {
         size(screen.width, screen.height);
         background(0);
         smooth();
+        Item item = new MockItem();
         for (int i = 0; i < 40; i++) {
             float x = (float) (Math.random() * screen.width);
             float y = (float) (Math.random() * screen.height);
             float r = (float) (Math.random() * 100);
-            this.elements.add(new Bubble(x, y, r));
+            Bubble bubble = new Bubble(item, x, y, r);
+            this.elements.add(bubble);
+            this.physicsEngine.add(bubble);
         }
+        (new Thread(physicsEngine)).start();
     }
     
     @Override
