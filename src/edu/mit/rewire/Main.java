c97b8748d433ab9c17931e756606ed4584eb0468
mmmmtang <mmmmtang@14876ee8-1428-11de-b413-eb0469710719>
2009-04-09 18:46:41 +0000

    Main can now run REWIRE full-screen, as a standalone application.

--- src/edu/mit/rewire/Main.java
+++ src/edu/mit/rewire/Main.java
@@ -1,12 +1,14 @@
 package edu.mit.rewire;
 
+import processing.core.PApplet;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        
-        // Launch the application from here...
-        
+
+        PApplet.main(new String[] { "--present",
+                "edu.mit.rewire.view.ProcessingView" });
+
     }
 
 }
