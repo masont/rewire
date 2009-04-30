4f06ba2187132d0d9fcb5008b7de7749368fc6fc
friendofrobots <friendofrobots@14876ee8-1428-11de-b413-eb0469710719>
2009-04-30 21:50:36 +0000

    Weather in a bubble.

--- trunk/src/edu/mit/rewire/model/WeatherItem.java
+++ trunk/src/edu/mit/rewire/model/WeatherItem.java
@@ -1,13 +1,13 @@
 package edu.mit.rewire.model;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
-import twitter4j.TwitterException;
 import twitter4j.http.HttpClient;
 
 public class WeatherItem implements Item {
@@ -17,6 +17,11 @@ public class WeatherItem implements Item {
     private String baseURL = "http://www.google.com/ig/api";
     private String zip;
     
+    private String body;
+    private String header;
+    private String title;
+    
+	@SuppressWarnings("unchecked")
 	public WeatherItem() {
         http = new HttpClient();
         this.zip = "02139";
@@ -25,16 +30,30 @@ public class WeatherItem implements Item {
     		SAXBuilder parser = new SAXBuilder();
 			Document doc = parser.build(baseURL + "?weather="+zip);
 			Element weather = doc.getRootElement().getChild("weather");
+			
+			Element forecast_information = weather.getChild("forecast_information");
+			String city = forecast_information.getChild("city").getAttributeValue("data");
+			
 			Element current_conditions = weather.getChild("current_conditions");
 			String condition = current_conditions.getChild("condition").getAttributeValue("data");
 			String temp_f = current_conditions.getChild("temp_f").getAttributeValue("data");
 			String humidity = current_conditions.getChild("humidity").getAttributeValue("data");
 			String wind_condition = current_conditions.getChild("wind_condition").getAttributeValue("data");
-			System.out.println("Weather in Cambridge, MA (02139)\n"
-					+ "Condition: " + condition +"\n"
-					+ "Temp: " + temp_f + "deg F\n"
-					+ humidity +"\n"
-					+ wind_condition);
+
+			List<Element> forecast_conditions = weather.getChildren("forecast_conditions");
+			
+			title = temp_f + "\u00B0F " + condition;
+			header = city;
+			body = "Today: " + temp_f + "\u00B0F. " + condition + ". " +
+				humidity + ". " + wind_condition + ".";
+			
+			for (Element forecast : forecast_conditions) {
+				body += "\n";
+				body += forecast.getChild("day_of_week").getAttributeValue("data") + ": ";
+				body += forecast.getChild("high").getAttributeValue("data") + "\u00B0F / ";
+				body += forecast.getChild("low").getAttributeValue("data") + "\u00B0F. ";
+				body += forecast.getChild("condition").getAttributeValue("data") + ".";
+			}
 		} catch (JDOMException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -45,22 +64,20 @@ public class WeatherItem implements Item {
 	
 	public String getBody() {
 		// TODO Auto-generated method stub
-		return null;
+		return body;
 	}
 
 	public String getHeader() {
 		// TODO Auto-generated method stub
-		return null;
+		return header;
 	}
 
 	public String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
+		return title;
 	}
 
 	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return "weather";
 	}
     
     
