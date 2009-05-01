b6e5418e98c1019694782a8a590f9e3d2c5103dd
jared.sadoian@gmail.com <jared.sadoian@gmail.com@14876ee8-1428-11de-b413-eb0469710719>
2009-05-01 03:26:22 +0000

    Screen grayout at a rudimentary level working.

---
+++ src/edu/mit/rewire/view/BackgroundOverlay.java
@@ -0,0 +1,57 @@
+package edu.mit.rewire.view;
+
+import edu.mit.rewire.controller.Controller;
+import processing.core.PGraphics;
+
+public class BackgroundOverlay implements Drawable, MouseAware {
+
+	private float width;
+	private float height;
+	private float alpha;
+	
+	public BackgroundOverlay(float width, float height) {
+		this.width = width;
+		this.height = height;
+		this.alpha = 10;
+	}
+	
+	@Override
+	public void draw(PGraphics graphics) {
+		graphics.fill(255, alpha);
+		graphics.rect(0, 0, width, height);
+		
+	}
+	
+	public void setAlpha(float alpha) {
+		this.alpha = alpha;
+	}
+	
+	public float getAlpha() {
+		return alpha;
+	}
+
+	@Override
+	public void dispatchClick(Controller controller, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispatchOver(Controller controller, int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getZ() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean hits(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
