98c5f34fb7156fccde929a4efb0a12b20286a7b9
mmmmtang <mmmmtang@14876ee8-1428-11de-b413-eb0469710719>
2009-05-05 08:48:39 +0000

    Mark read button now arbitrarily rendered at 100,100.

---
+++ trunk/src/edu/mit/rewire/view/MarkReadButton.java
@@ -0,0 +1,20 @@
+package edu.mit.rewire.view;
+
+public class MarkReadButton extends Button {
+    
+    private final Bubble bubble;
+    
+    public MarkReadButton(Bubble bubble, float x, float y, float height, float width) {
+        super(x, y, height, width);
+        this.bubble = bubble;        
+        this.upImage = this.overImage = new Shape("popButton", x, y, height, width);
+        this.downImage = new Shape("popButtonClick", x, y, height, width);
+    }
+
+    public void execute(ProcessingView view, int x, int y) {
+        view.removeMouseAware(this);
+        view.removeDrawable(this);
+        view.markRead(bubble);
+    }
+
+}
