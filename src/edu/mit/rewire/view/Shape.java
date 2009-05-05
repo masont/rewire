b3fb37d2a4c7bf772f0fed964572f82dd981ff65
mmmmtang <mmmmtang@14876ee8-1428-11de-b413-eb0469710719>
2009-05-05 05:37:41 +0000

    Fun times!

---
+++ src/edu/mit/rewire/view/Shape.java
@@ -0,0 +1,13 @@
+package edu.mit.rewire.view;
+
+import processing.core.PGraphics;
+
+public class Shape implements Drawable {
+
+    @Override
+    public void draw(PGraphics graphics) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
