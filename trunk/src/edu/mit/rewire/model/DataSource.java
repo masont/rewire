f9a86a882a9cb237cd42358a36b6986766b89477
friendofrobots <friendofrobots@14876ee8-1428-11de-b413-eb0469710719>
2009-05-05 16:39:08 +0000

    It is my hope that we now have one RSS feed in rewire. It is likely poorly formatted. Someone please let me know.

--- trunk/src/edu/mit/rewire/model/DataSource.java
+++ trunk/src/edu/mit/rewire/model/DataSource.java
@@ -1,7 +1,16 @@
 package edu.mit.rewire.model;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -11,9 +20,12 @@ public class DataSource {
 	
 	private List<Item> items;
 
+	@SuppressWarnings("unchecked")
 	public DataSource() {
 		this.items = new ArrayList<Item>();
+		// WEATHER
 		this.addItem(new WeatherItem());
+		// TWITTER 
 		Twitter twitter = new Twitter("claytonrules","superawesome");
 		List<Status> timeline = new ArrayList<Status>();
 		try {
@@ -25,46 +37,35 @@ public class DataSource {
 			Status status = timeline.get(i);
 			this.addItem(new TwitterItem(status));
 		}
-		
+		// EMAIL
 		Inbox inbox = new Inbox();
-
 		try {
 			for (int i=0; i<5; i++) {
-				this.addItem(inbox.getEmail(i));
+				this.addItem(new EmailItem(inbox.getEmail(i)));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-//		try {
-//			SAXBuilder parser = new SAXBuilder();
-//			Document doc = parser.build("http://www.google.com/calendar/m");
-//		} catch (JDOMException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+        try {
+        	SyndFeedInput input = new SyndFeedInput();
+			SyndFeed feed = input.build(new XmlReader(new URL("http://feeds.boingboing.net/boingboing/iBag")));
+			List<SyndEntry> entries = feed.getEntries();
+
+			for (int i=0; i<5; i++) {
+				this.addItem(new RSSItem(entries.get(i)));
+			}
+			
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (FeedException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-        this.addItem(new DummyItem("rss",
-                "Heiko Smith",
-                "6 hours ago",
-        "accepted your friend request."));
-        this.addItem(new DummyItem("rss",
-                "Anna Smith",
-                "20 hours ago",
-        "knows a secret about you."));
-        this.addItem(new DummyItem("rss",
-                "Greg Smith",
-                "on Thursday",
-        "wrote on your Wall."));
-        this.addItem(new DummyItem("rss",
-                "John Smith",
-                "9:28am",
-        "commented on your status"));
-        this.addItem(new DummyItem("rss",
-                "Amy Smith",
-                "7:48pm",
-        "made a comment about your photo."));
         this.addItem(new DummyItem("todo",
                 "Agenda",
                 "4/10/09",
--- trunk/src/edu/mit/rewire/model/EmailItem.java
+++ trunk/src/edu/mit/rewire/model/EmailItem.java
@@ -1,17 +1,67 @@
 package edu.mit.rewire.model;
 
+import java.io.IOException;
+import java.util.Date;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+
 
 public class EmailItem implements Item {
-	
+
+	private Message message;
+
 	private String body;
 	private String header;
 	private String title;
-	
+
+	public EmailItem(Message m) {
+		this.message = m;
+		Address[] a;
+		try {
+			// FROM
+			String from = "from";
+			if ((a = m.getFrom()) != null) {
+				for (int j = 0; j < a.length; j++) {
+					InternetAddress ia = (InternetAddress)a[j];
+					from = ia.getPersonal();
+					if (from == null) {
+						ia.toString();
+					}
+				}
+			}
+			// TO
+			String to = "to";
+			if ((a = m.getRecipients(Message.RecipientType.TO)) != null) {
+				for (int j = 0; j < a.length; j++) {
+					to = ("TO: " + a[j].toString());
+				}
+			}
+			// SUBJECT
+			String subject = m.getSubject();
+			// DATE
+			Date d = m.getSentDate();
+			String date = d != null ? d.toString() : "unknown";
+			// CONTENT
+			String content = m.getContent().toString();
+
+			this.title = subject;
+			this.header = from;
+			this.body = to + "\n" + content + "\n" + date;
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	public EmailItem(String title, String header, String body) {
 		this.title = title;
 		this.header = header;
 		this.body = body;
-		
 	}
 
 	public String getBody() {
@@ -28,6 +78,10 @@ public class EmailItem implements Item {
 
 	public String getType() {
 		return "email";
+	}
+
+	public Message getMessage() {
+		return message;
 	}
 
 }
